df60ea9a153e3cce037a240f0ed1d7ad9c45663b
Enrique Peláez <e.pelaez@alumnos.upm.es>
2021-11-02 14:19:23 +0100

    Create an endpoint to check the api status

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+from fastapi import FastAPI
+from src.service_info.entrypoints import service_info_router
 
+
+app = FastAPI()
+app.include_router(service_info_router.router)
 
 if __name__ == '__main__':
     print('Hello World')
