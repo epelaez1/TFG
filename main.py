ee1b2bfc0c58dc3b69b79a2349ade5dd64e94ec3
Enrique Peláez <e.pelaez@alumnos.upm.es>
2021-11-03 09:09:33 +0100

    Remove hello world from main file

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from fastapi import FastAPI
-from src.service_info.entrypoints import service_info_router
 
+from src.service_info.entrypoints import service_info_router
 
 app = FastAPI()
 app.include_router(service_info_router.router)
-
-if __name__ == '__main__':
-    print('Hello World')
