1a929a9d02341523288abcdadfd02196c62ff624
Enrique Peláez <e.pelaez@alumnos.upm.es>
2021-11-05 15:08:30 +0100

    Make the user repository available for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,8 +2,15 @@ import pytest
 from fastapi.testclient import TestClient
 
 from main import app
+from src.user.domain.user_repository import UserRepository
+from src.user.domain.user_repository import BasicUserRepository
 
 
 @pytest.fixture
 def app_client() -> TestClient:
     return TestClient(app)
+
+
+@pytest.fixture
+def user_repository() -> UserRepository:
+    return BasicUserRepository()
