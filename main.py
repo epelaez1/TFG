01ddd7ca3bf9369f5dfb815a883aa4cfe537c3d5
Enrique Peláez <e.pelaez@alumnos.upm.es>
2021-11-08 16:20:03 +0100

    Include the user router in the app

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 
 from src.service_info.entrypoints import service_info_router
+from src.user.entrypoints import user_router
 
 app = FastAPI()
 app.include_router(service_info_router.router)
+app.include_router(user_router.router)
