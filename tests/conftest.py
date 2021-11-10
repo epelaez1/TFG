fc04eae7264a06a8cd6d30ff3f5b9c603b746113
Enrique Peláez <e.pelaez@alumnos.upm.es>
2021-11-10 14:04:07 +0100

    Create a user repository using mongo client and test it

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,8 +2,13 @@ import pytest
 from fastapi.testclient import TestClient
 
 from main import app
+from src.config import mongo_settings
+from src.mongo_client import MongoDBClient
 from src.user.domain.user_repository import BasicUserRepository
 from src.user.domain.user_repository import UserRepository
+from src.user.storage.mongo_user_repository import UserMongoDB
+
+TEST_DB_NAME = 'tfg_testing_db'
 
 
 @pytest.fixture
@@ -23,3 +28,10 @@ def user_sample() -> dict[str, str]:
         'name': 'bob',
         'phone': '6543213210',
     }
+
+
+@pytest.fixture
+def mongo_user_repository():
+    with MongoDBClient(uri=mongo_settings.uri, database=TEST_DB_NAME) as db_client:
+        yield UserMongoDB(client=db_client)
+        db_client.drop_test_database(TEST_DB_NAME)
