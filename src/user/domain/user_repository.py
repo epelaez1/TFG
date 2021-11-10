c9f8fd96fa8b354e2af7ad6aa6495f390ab136fa
Enrique Peláez <e.pelaez@alumnos.upm.es>
2021-11-10 15:53:32 +0100

    Remove unused statement

--- src/user/domain/user_repository.py
+++ src/user/domain/user_repository.py
@@ -2,7 +2,6 @@ from abc import ABC
 from abc import abstractmethod
 
 from src.user.domain.user import User
-from src.user.domain.user_exceptions import UserAlreadyRegistered
 
 
 class UserRepository(ABC):
@@ -22,10 +21,7 @@ class BasicUserRepository(UserRepository):
         self.users: dict[str, User] = {}
 
     def add(self, user: User) -> None:
-        if self.has(user.email):
-            raise UserAlreadyRegistered
-        else:
-            self.users[user.email] = user
+        self.users[user.email] = user
 
     def has(self, email: str) -> bool:
         return email in self.users
