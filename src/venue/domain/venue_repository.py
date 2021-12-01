5639388e6698c8e2c5b76cf82f01abc05a176604
Enrique Peláez <e.pelaez@alumnos.upm.es>
2021-12-01 05:07:43 +0100

    Work always with the venue id as str

--- src/venue/domain/venue_repository.py
+++ src/venue/domain/venue_repository.py
@@ -1,8 +1,6 @@
 from abc import ABC
 from abc import abstractmethod
 
-from bson import ObjectId
-
 from src.venue.domain.exceptions import VenueDoesNotExist
 from src.venue.domain.venue import PrivateSpot
 from src.venue.domain.venue import Venue
@@ -11,7 +9,7 @@ from src.venue.domain.venue import Venue
 class VenueRepository(ABC):
 
     @abstractmethod
-    def has(self, id_: ObjectId | str) -> bool:
+    def has(self, id_: str) -> bool:
         raise NotImplementedError
 
     @abstractmethod
@@ -19,15 +17,15 @@ class VenueRepository(ABC):
         raise NotImplementedError
 
     @abstractmethod
-    def get(self, id_: ObjectId | str) -> Venue:
+    def get(self, id_: str) -> Venue:
         raise NotImplementedError
 
     @abstractmethod
-    def get_private_spot_numbers(self, id_: ObjectId | str) -> set[int]:
+    def get_private_spot_numbers(self, id_: str) -> set[int]:
         raise NotImplementedError
 
     @abstractmethod
-    def add_private_spot(self, id_: ObjectId | str, private_spot: PrivateSpot) -> None:
+    def add_private_spot(self, id_: str, private_spot: PrivateSpot) -> None:
         raise NotImplementedError
 
 
@@ -39,23 +37,23 @@ class BasicVenueRepository(VenueRepository):
     def add(self, venue: Venue) -> None:
         self.venues[str(venue.id)] = venue
 
-    def has(self, id_: ObjectId | str) -> bool:
-        return str(id_) in self.venues
+    def has(self, id_: str) -> bool:
+        return id_ in self.venues
 
-    def get(self, id_: ObjectId | str) -> Venue:
-        venue = self.venues.get(str(id_))
+    def get(self, id_: str) -> Venue:
+        venue = self.venues.get(id_)
         if venue is None:
             raise VenueDoesNotExist
         return venue
 
-    def get_private_spot_numbers(self, id_: ObjectId | str) -> set[int]:
-        if str(id_) not in self.venues:
+    def get_private_spot_numbers(self, id_: str) -> set[int]:
+        if id_ not in self.venues:
             raise VenueDoesNotExist()
-        return self.venues[str(id_)].private_spot_numbers
+        return self.venues[id_].private_spot_numbers
 
-    def add_private_spot(self, id_: ObjectId | str, private_spot: PrivateSpot) -> None:
-        if str(id_) not in self.venues:
+    def add_private_spot(self, id_: str, private_spot: PrivateSpot) -> None:
+        if id_ not in self.venues:
             raise VenueDoesNotExist()
 
-        self.venues[str(id_)].private_spot_numbers.add(private_spot.spot_number)
-        self.venues[str(id_)].private_spots.append(private_spot)
+        self.venues[id_].private_spot_numbers.add(private_spot.spot_number)
+        self.venues[id_].private_spots.append(private_spot)
